78f740ad2f4bdb246f3f766e4459bf4751be9be8
rmessett15 <rmessett15@gmail.com>
2023-04-04 20:55:09 -0500

    modified generateMarkdown file, added some comments and formatted page and got rid of usused code

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,7 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
+// This function is what generates the markdown file for README.md doc to be generated
 function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+  return `# ${data.title}`;
 }
 
+// Exporting generateMarkdown function
 module.exports = generateMarkdown;
